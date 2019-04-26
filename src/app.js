374391f6138891c34dd574883d063e3feb09d58d
leny <info@flatland.be>
2019-04-26 20:46:46 +0200

    Minimal svelte app source code

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/* leny/meet-svelte
+ *
+ * /src/app.js - Entry point for svelte app
+ *
+ * coded by leny@flatLand!
+ * started at 26/04/2019
+ */
+
+import App from "./components/app.svelte";
+
+const app = new App({
+    target: document.body,
+});
