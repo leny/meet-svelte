c4afe35ea92d9efe556b87163f79bd91f6742914
leny <info@flatland.be>
2019-04-27 12:16:21 +0200

    Implement logical each block

--- src/components/app.svelte
+++ src/components/app.svelte
@@ -1,6 +1,15 @@
 {#if showInfo}
     <Nested answer={42} />
     <Info {...pkg} />
+
+    <ul>
+        {#each cats as cat (cat.id)}
+            <li>
+                <strong>{cat.name}:</strong>
+                <span>{nowYear - cat.birthYear}yo</span>
+            </li>
+        {/each}
+    </ul>
 {:else}
     <p>Nothing to see here!</p>
 {/if}
@@ -21,4 +30,11 @@
     let showInfo = false;
 
     const toggle = () => (showInfo = !showInfo);
+
+    const cats = [
+        {id: "skitty", name: "Skitty", birthYear: 2010},
+        {id: "pixel", name: "Pixel", birthYear: 2013},
+    ];
+
+    const nowYear = new Date().getFullYear();
 </script>
